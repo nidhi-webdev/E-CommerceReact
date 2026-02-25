b372ccdbdebf5c74ff855bec91cce1e32308a10d
nidhi <kumarinidhi0594@gmail.com>
2026-02-25 18:42:51 +0100

    Router with component

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -4,7 +4,10 @@ import Home from "./components/Home"
 const App = () => {
 
   const router = createBrowserRouter([
-    { path: "/", element: <Home />}
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> },
+    { path: "/movie", element: <Movie /> },
+    { path: "/contact", element: <Contact /> }
   ])
 
   return <h1> Hello React Router </h1>
