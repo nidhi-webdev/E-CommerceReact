09d501614e1da76af8cca2d255ac02347386194b
nidhi <kumarinidhi0594@gmail.com>
2026-02-26 16:31:07 +0100

    Router and outlet

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -3,14 +3,22 @@ import Home from "./pages/Home"
 import Contact from "./pages/Contact"
 import Movie from "./pages/Movie"
 import About from "./pages/About"
+import { AppLayout } from "./components/layout/AppLayout"
 
 const App = () => {
 
   const router = createBrowserRouter([
-    { path: "/", element: <Home /> },
-    { path: "/about", element: <About /> },
-    { path: "/movie", element: <Movie /> },
-    { path: "/contact", element: <Contact /> }
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/about", element: <About /> },
+        { path: "/movie", element: <Movie /> },
+        { path: "/contact", element: <Contact /> }
+      ]
+    }
+
   ])
 
   return <RouterProvider router={router} />
