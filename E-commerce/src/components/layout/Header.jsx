04e7d8e8f2be1f0e695e78731b0391454646921e
nidhi <kumarinidhi0594@gmail.com>
2026-02-26 21:04:52 +0100

    design

--- E-commerce/src/components/layout/Header.jsx
+++ E-commerce/src/components/layout/Header.jsx
@@ -1,24 +1,24 @@
 export const Header = () => {
     return (
-        <div>
-            <div className="bg-gray-800 text-white">
+        <div className="ml-5 mr-5">
+            <div className="bg-gray-800 text-white flex justify-around p-5 ">
                 <h1> Get membership, 30-days return or rufund Guarantee </h1>
-                <div>
-                    <a> SIGN IN </a>
-                    <a> SIGN OUT  </a>
+                <div className="flex gap-5 cursor-pointer">
+                    <a href="#"> SIGN IN </a>
+                    <a href="#"> SIGN OUT  </a>
                 </div>
             </div>
 
-            <div>
-                <h1> Filx </h1>
-                <div> 
-                    <ul>
+            <div className="bg-amber-100 flex justify-around items-center p-4 shadow-amber-400">
+                <h1 className="text-2xl font-bold"> Filx </h1>
+                <nav> 
+                    <ul className="flex gap-8">
                         <li> <a href=""> Home </a></li>
                         <li> <a href=""> About </a></li>
                         <li> <a href=""> Movie </a></li>
                         <li> <a href=""> Contact  </a></li>
                     </ul>
-                </div>
+                </nav>
             </div>
 
             
