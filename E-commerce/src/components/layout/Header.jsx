41f07767d7c1da7800936fe13a1f9062147a60aa
nidhi <kumarinidhi0594@gmail.com>
2026-02-26 21:12:31 +0100

    design

--- E-commerce/src/components/layout/Header.jsx
+++ E-commerce/src/components/layout/Header.jsx
@@ -1,22 +1,24 @@
+import { NavLink } from "react-router-dom"
+
 export const Header = () => {
     return (
         <div className="ml-5 mr-5">
             <div className="bg-gray-800 text-white flex justify-around p-5 ">
                 <h1> Get membership, 30-days return or rufund Guarantee </h1>
                 <div className="flex gap-5 cursor-pointer">
-                    <a href="#"> SIGN IN </a>
-                    <a href="#"> SIGN OUT  </a>
+                    <NavLink to="#"> SIGN IN </NavLink>
+                    <NavLink to="#"> SIGN OUT  </NavLink>
                 </div>
             </div>
 
             <div className="bg-amber-100 flex justify-around items-center p-4 shadow-amber-400">
                 <h1 className="text-2xl font-bold"> Filx </h1>
                 <nav> 
-                    <ul className="flex gap-8">
-                        <li> <a href=""> Home </a></li>
-                        <li> <a href=""> About </a></li>
-                        <li> <a href=""> Movie </a></li>
-                        <li> <a href=""> Contact  </a></li>
+                    <ul className="flex gap-8 cursor-pointer">
+                        <li> <NavLink to=""> Home </NavLink></li>
+                        <li> <NavLink to=""> About </NavLink></li>
+                        <li> <NavLink to=""> Movie </NavLink></li>
+                        <li> <NavLink to=""> Contact  </NavLink></li>
                     </ul>
                 </nav>
             </div>
