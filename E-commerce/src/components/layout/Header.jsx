e4b43b307acf87e094743f76a183c8530eda352b
nidhi <kumarinidhi0594@gmail.com>
2026-02-26 20:55:59 +0100

    Skelton and design

--- E-commerce/src/components/layout/Header.jsx
+++ E-commerce/src/components/layout/Header.jsx
@@ -1,3 +1,27 @@
 export const Header = () => {
-    return <h1> Header </h1>
+    return (
+        <div>
+            <div className="bg-gray-800 text-white">
+                <h1> Get membership, 30-days return or rufund Guarantee </h1>
+                <div>
+                    <a> SIGN IN </a>
+                    <a> SIGN OUT  </a>
+                </div>
+            </div>
+
+            <div>
+                <h1> Filx </h1>
+                <div> 
+                    <ul>
+                        <li> <a href=""> Home </a></li>
+                        <li> <a href=""> About </a></li>
+                        <li> <a href=""> Movie </a></li>
+                        <li> <a href=""> Contact  </a></li>
+                    </ul>
+                </div>
+            </div>
+
+            
+        </div>
+    )
 }
