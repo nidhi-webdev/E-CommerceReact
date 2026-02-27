50175efc0d68849daeaa6f7b2d3d1b7d13e2301e
nidhi <kumarinidhi0594@gmail.com>
2026-02-27 10:36:57 +0100

    Error page

--- E-commerce/src/pages/ErrorPage.jsx
+++ E-commerce/src/pages/ErrorPage.jsx
@@ -1,9 +1,22 @@
-import { useRouteError } from "react-router-dom"
+import { NavLink, useRouteError } from "react-router-dom"
 
 export const ErrorPage = () => {
     const error = useRouteError()
     console.log(error);
 
 
-    return <h1> 404 Error Page </h1>
+    return (
+        <div className="flex flex-col justify-center items-center min-h-screen gap-3">
+            <figure> <img src="https://cdn.dribbble.com/users/722246/screenshots/3066818/404-page.gif" alt="404 Page" /></figure>
+            <div className="flex flex-col justify-center items-center">
+                <h1 className="font-bold text-xl"> The page yor were looking for could not be found   </h1>
+                <p className="text-xl"> Back to previous page </p>
+            </div>
+
+            <NavLink to="/" className={`bg-red-700 px-4 py-2 rounded text-white`}>
+                Go Back to Home page
+            </NavLink>
+        </div>
+
+    )
 }
