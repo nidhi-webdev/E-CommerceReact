cb3693b92c9e72fd82a598d82a1492b348be501c
nidhi <kumarinidhi0594@gmail.com>
2026-02-28 11:02:44 +0100

    Mapping all data

--- E-commerce/src/pages/Movie.jsx
+++ E-commerce/src/pages/Movie.jsx
@@ -4,12 +4,14 @@ import { useLoaderData } from 'react-router-dom'
 const Movie = () => {
   const movieData = useLoaderData()
   console.log(movieData);
-  
+
   return (
     <div>
-      <h1></h1>
+      <h1> Movie Page </h1>
       {
-
+        movieData.Search.map((curMovie) => {
+          return <Card key={curMovie.imdbID} curMovie = {curMovie}   />
+        })
       }
     </div>
   )
