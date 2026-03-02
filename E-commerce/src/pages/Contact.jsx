d8d6f39f0c8eb6e1d2087a172ba95e61e942fcb0
nidhi <kumarinidhi0594@gmail.com>
2026-03-02 22:32:34 +0100

    Design

--- E-commerce/src/pages/Contact.jsx
+++ E-commerce/src/pages/Contact.jsx
@@ -2,19 +2,21 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>
+    <div className='ml-7 flex flex-col  w-1/4'>
       <h1> Contact Us  </h1>
+
       <p> Get in touch with us We are always hero to help you. </p>
+
       <label> Full Name </label>
       <input type='text' name='fullName' placeholder='Enter Your Full Name' />
+
       <label> Email Address </label>
       <input type='email' name='email' placeholder='Enter Your Email Address ' />
+
       <label> Message </label>
       <textarea type='text' name='textarea' placeholder='We are always here to help you' > </textarea>
-
-
-
-
+      
+      <button className='bg-'> Send Message </button>
     </div>
   )
 }
