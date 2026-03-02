7b65507d0da6a10e1822740107ac65af61e9590b
nidhi <kumarinidhi0594@gmail.com>
2026-03-02 22:48:18 +0100

    Tailwind design

--- E-commerce/src/pages/Contact.jsx
+++ E-commerce/src/pages/Contact.jsx
@@ -2,21 +2,24 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='ml-7 flex flex-col  w-1/4'>
-      <h1> Contact Us  </h1>
+    <div className='ml-7 flex flex-col  w-1/3 text-gray-600 font-serif'>
+      <h1 className='text-2xl font-extrabold mt-10 '> Contact Us  </h1>
 
-      <p> Get in touch with us We are always hero to help you. </p>
+      <p className='mb-8'> Get in touch with us We are always hero to help you. </p>
 
-      <label> Full Name </label>
-      <input type='text' name='fullName' placeholder='Enter Your Full Name' />
+      <label className='mb-1 text-sm font-semibold'> Full Name </label>
+      <input type='text' name='fullName' placeholder='Enter Your Full Name' 
+      className='border border-gray-300 mb-5 outline-none p-2 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all' />
 
-      <label> Email Address </label>
-      <input type='email' name='email' placeholder='Enter Your Email Address ' />
+      <label className='mb-1 text-sm font-semibold'> Email Address </label>
+      <input type='email' name='email' placeholder='Enter Your Email Address ' 
+      className='border border-gray-300 mb-5 outline-none p-2 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all' />
 
-      <label> Message </label>
-      <textarea type='text' name='textarea' placeholder='We are always here to help you' > </textarea>
-      
-      <button className='bg-'> Send Message </button>
+      <label className='mb-1 text-sm font-semibold'> Message </label>
+      <textarea name='textarea' placeholder='We are always here to help you' 
+      className='border border-gray-300 mb-5 outline-none p-4 focus:border-blue-500 focus:ring-1 focus:ring-blue-500 transition-all' > </textarea>
+
+      <button className='bg-gray-800 text-white px-4 py-3 rounded cursor-pointer hover:bg-red-900 transition-all hover:scale-105 active:scale-95 font-bold uppercase tracking-wider '> Send Message </button>
     </div>
   )
 }
