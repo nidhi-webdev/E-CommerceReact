0ef9fa8a4b0d1db789245026d6fb7813db7a9d9d
nidhi <kumarinidhi0594@gmail.com>
2026-03-02 17:36:56 +0100

    Contact sekeleton

--- E-commerce/src/pages/Contact.jsx
+++ E-commerce/src/pages/Contact.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      Contact page
+      <h1> Contact Us  </h1>
+      <p> Get in touch with us We are always hero to help you. </p>
+      <label> Full Name </label>
+      <input type='text' name='fullName' placeholder='Enter Your Full Name' />
+      <label> Email Address </label>
+      <input type='email' name='email' placeholder='Enter Your Email Address ' />
+      <label> Message </label>
+      <textarea type='text' name='textarea' placeholder='We are always here to help you' > </textarea>
+
+
+
+
     </div>
   )
 }
